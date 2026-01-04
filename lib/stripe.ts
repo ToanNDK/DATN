ac5b1a133d00848c23c6f5028aa74a21877d30e8
Hello <Hello@TOANNDK>
2026-01-04 20:46:20 +0700

    update version React

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,7 +4,7 @@ if (!process.env.STRIPE_SECRET_KEY){
     throw new Error ("STRIPE_SECRET_KET is not defined");
 }
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ,{
-    apiVersion:"2025-09-30.clover",
+    apiVersion:"2025-10-29.clover",
 })
 
 export default stripe

fdc99dfce5397aa6caf45be635e02498b0161635
Hello <Hello@TOANNDK>
2026-01-19 23:13:18 +0700

    move zalo.png to public

--- components/ZaloButton.tsx
+++ components/ZaloButton.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { zalo } from "@/images";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-const ZALO_LINK = "https://zalo.me/0813581390"; 
+const ZALO_LINK = "https://zalo.me/0813581390";
 
 export default function ZaloButton() {
   return (
@@ -17,13 +16,7 @@ export default function ZaloButton() {
       className="fixed bottom-28 right-6 z-50 w-14 h-14 rounded-full shadow-lg bg-white flex items-center justify-center"
       aria-label="Liên hệ Zalo"
     >
-      <Image
-        src={zalo}
-        alt="Zalo"
-        width={36}
-        height={36}
-        priority
-      />
+      <Image src="/zalo.png" alt="Zalo" width={36} height={36} />
     </motion.a>
   );
 }

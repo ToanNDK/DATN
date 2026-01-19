b491eb5e1cceccd840d0e35cbb8843bb765db714
Hello <Hello@TOANNDK>
2026-01-19 22:43:38 +0700

    update

--- images/index.ts
+++ images/index.ts
@@ -4,3 +4,4 @@ import banner_1 from "./banner/banner_1.png";
 import banner_2 from "./banner/banner_2.png";
 import zalo from "./zalo.png";
 export { paypalLogo, emptyCart, banner_1, banner_2,zalo};
+ 
